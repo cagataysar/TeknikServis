37bd8d6f3ab6256b0d82d30768f3546c5912b159
Emre Çelen <79768707+emrcelen@users.noreply.github.com>
2023-02-01 03:53:07 +0300

    SaleService güncellendi.

--- src/main/java/com/garanti/TeknikServis/service/SaleService.java
+++ src/main/java/com/garanti/TeknikServis/service/SaleService.java
@@ -1,9 +1,16 @@
 package com.garanti.TeknikServis.service;
 
+import com.garanti.TeknikServis.excepton.EntityNoContentException;
+import com.garanti.TeknikServis.excepton.UnexpectedException;
 import com.garanti.TeknikServis.model.Sale;
+import com.garanti.TeknikServis.model.SaleDto;
 import com.garanti.TeknikServis.repo.SaleRepository;
+import com.garanti.TeknikServis.repo.UserRepo;
+import com.garanti.TeknikServis.security.TokenParser;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,6 +19,7 @@ import java.util.List;
 public class SaleService {
 
     private SaleRepository repository;
+    private UserRepo userRepo;
 
     public List< Sale > getAll() {
         return repository.getAll();
@@ -30,5 +38,36 @@ public class SaleService {
         catch (Exception e) {
             return false;
         }
+    }
+
+    // User Servis Kısmı:
+    public List <SaleDto> getListofSales(){
+        List<SaleDto> list = repository.getListofSales();
+        if(!list.isEmpty())
+            return list;
+        throw new EntityNoContentException("Sistemde satışa uygun ürün bulunmamaktadır.");
+    }
+    public List <SaleDto> getListofSalesByProduct(String productType){
+        List<SaleDto> list = repository.getListofSalesByProduct(productType);
+        if(!list.isEmpty())
+            return list;
+        throw new EntityNoContentException("Sistemde satışa uygun ürün bulunmamaktadır.");
+    }
+    @Transactional
+    public String buyTheProductInAd(Integer id, String creditcard, HttpHeaders headers){
+        if(id != null && creditcard != null && creditcard.length() == 16){
+            String username = TokenParser.jwt(headers.get("Authorization").get(0).substring(7));
+            int userid = userRepo.getUserId(username);
+            if(repository.buyTheProductInAd(id)){
+                if(repository.insertSaleToSaleLog(id,userid,creditcard))
+                    return "Başarılı bir şekilde ürünü satın aldınız.";
+                throw new UnexpectedException("Ürün satın alınırken bir hata ile karşılaşıldı");
+            }
+            throw new IllegalArgumentException("Satın almaya çalıştığınız ürün sistemde bulunamadı.");
+
+        }
+        throw new IllegalArgumentException("İşlem gerçekleştirilirken bir hata ile karşılaşıldı.");
+
+
     }
 }
